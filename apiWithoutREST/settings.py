2d7de01c04075f691b26b64a3a3e93ed7e9eabf1
mehedihassan <mehedi_45seafarer@yahoo.com>
2020-08-26 18:19:39 -0400

    Added postgreSQL database conf.

--- apiWithoutREST/settings.py
+++ apiWithoutREST/settings.py
@@ -82,10 +82,28 @@ WSGI_APPLICATION = 'apiWithoutREST.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'mydb',
+
+        'USER': '',
+
+        'PASSWORD': '',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
+
 }
 
 # Password validation
